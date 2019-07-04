03dc6bf126dbd8a827e3828f0e9622bc91ac00db
SilviuIordache <silviu.iordache92@gmail.com>
2019-07-04 15:26:13 +0300

    working with 100,000 lines

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const Schema = mongoose.Schema;
 
 // create database
 mongoose.connect('mongodb://localhost:27017/mediaDB');
-mongoose.set('debug', true);
 
+
+let noOfMediaToRead = 100000;
+let mediaBufferSize = 25000;
+let mediaBuffer = []; // buffer used to insert data in big batches
 
 // create media schema
 let mediaSchema = new Schema({
@@ -16,16 +19,13 @@ let mediaSchema = new Schema({
   mediaType: {type: String, required: true},
   year: {type: String, required: true}
 });
-// create media model
+// create mongoose model
 const MediaEntry = mongoose.model('MediaEntry', mediaSchema);
-
 
 // local variables
 let t0 = new Date().getTime();
 let line_no = 0;
-let mediaCount = {}
 
-// create instance of readline
 // each instance is associated with single input stream
 let rl = readline.createInterface({
   input: fs.createReadStream('checkouts-by-title.csv')
@@ -33,24 +33,10 @@ let rl = readline.createInterface({
 });
 
 
-//Counts occurences of an object's keys inside an array and stores them to the corresponding key;
-let countOccurences = (arr, countObj) => {
-  // regex to trim the extra " and ' characters
-  mediaName = arr[2].replace(/("|')/g, "");
-  // dynamically fill the countObj with keys as we find them
-  if (countObj.hasOwnProperty(mediaName)) {
-    //if the key exists, we increment it
-    countObj[mediaName]++;
-  } else { 
-    //otherwise we add the new key to the obj with value 1
-    countObj[mediaName] = 1;
-  }
-}
-
-
 // event is emitted after each line
 rl.on('line', function (line) {
   
+  // feedback logs
   if (line_no % 10000 == 0) {
     console.log(`${line_no} lines processed`);
   }
@@ -58,7 +44,9 @@ rl.on('line', function (line) {
   //split lines by commas
   let arr = line.split(',');
 
-  if (line_no <= 1000) {
+  // Define line limit (to be stored in the DB)
+  if (line_no <= noOfMediaToRead) {
+
     //todo 1: construct an object (with scheme) from each line
     let mediaEntry = new MediaEntry({
       type: arr[0],
@@ -67,47 +55,39 @@ rl.on('line', function (line) {
       year: arr[3]
     })
 
-    //todo 2: push it to the mongoDB
-    mediaEntry.save((err, res) => {
-      if (err)
-        return console.error(err);
-      //console.log(res);
-      // closing the db connection
-      // mongoose.disconnect((err) => {
-      //   if (err)
-      //     return console.error(err);
-      //   console.log('Disconnected from mongodb.');
-      // });
-    });
-  } else { // stop reading from stream after line limit
-    
-    console.log('File reading stream terminated');
-    // Process duration 
-    let t2 = new Date().getTime();
-    console.log(`PROCESS DURATION: ${(t2-t0) / 1000} s `);
+    // read 25000 lines and save them to the buffer
+    if (!(line_no % mediaBufferSize == 0)) {
+      mediaBuffer.push(mediaEntry);
+    } else {
+      // push batch to the DB
+      MediaEntry.insertMany(mediaBuffer, () => {
+        console.log('inserted 1 batch');
+      })
 
+      //empty the buffer
+      mediaBuffer = [];
+    }
+
+  } else { 
+    // STOP READING
     rl.close();
   }
 
   //increment lines read number
   line_no++;
-  
-  
-  // add occurences from each line (in the array format)
-  countOccurences(arr, mediaCount);
 });
-
 
 
 // End of read stream event
 rl.on('close', 
-  function (line) {
+  function () {
+    console.log('Stopped reading from file');
 
     // Process duration 
-    let t1 = new Date().getTime();
-    console.log(`PROCESS DURATION: ${(t1-t0) / 1000} s `);
+    let t2 = new Date().getTime();
+    console.log(`PROCESS DURATION: ${(t2-t0) / 1000} s `);
 
     // Total lines count
-    console.log(`Lines total : ${line_no}`);
+    console.log(`processed lines:  ${line_no}`);
   }
 );

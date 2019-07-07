4c360fe463a544e6f841aace8532b47599e8a5b4
Silviu <silviu.iordache92@gmail.com>
2019-07-07 20:05:35 +0300

    progress

--- MediaFlow/mediaFlow.js
+++ MediaFlow/mediaFlow.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 // import the mongo client
 const mdbclient = require('mongodb').MongoClient;
 
+
+
 // connect to the local db
 const url = 'mongodb://localhost:27017/';
 const config = require('./config.js');
@@ -13,7 +15,7 @@ const filePath = '../testFiles/checkouts-by-title.csv';
 let rl;
 const t0 = new Date().getTime();
 let line_no = 0;
-const noOfMediaToRead = 250007;
+const noOfMediaToRead = 250010;
 const chunkSize = 25000;
 let chunksTotal = Math.ceil(noOfMediaToRead/chunkSize);
 let chunkNo = 0;
@@ -27,7 +29,6 @@ function parseLine(line) {
 
 	// define line limit (to be stored in the DB)
 	if (line_no <= noOfMediaToRead) {
-
 		// grabbing field names from the first line in the csv
 		if (line_no === 0) {
 			// using rest parameter in order to pass the values from arr by value
@@ -39,18 +40,16 @@ function parseLine(line) {
 			}
 			// add object to the buffer
 			mediaBuffer.push(mediaObj);
-
 			// clear media object
 			mediaObj = {};
-
-			// If buffer reached the chunkSize, pause to insert it to the DB
+			// if buffer reached the chunkSize, pause to insert it to the DB
 			if (mediaBuffer.length % chunkSize == 0) {
 				rl.pause();
 			}
 		}
-	} else if (mediaBuffer > 0){
-		rl.pause();
-	} else {
+  } else if (mediaBuffer.length > 0){
+	  rl.pause();
+  } else {
 		rl.close();
 	}
 
@@ -58,30 +57,16 @@ function parseLine(line) {
 }
 
 function endActions(col, conn) {
-
-	console.log(mediaBuffer.length);
-
+  console.log('ending file stream');
 	// add remaining chunk, if any
 	if (mediaBuffer.length > 0) {
-		//console.log('echo');
-		insertChunk(col, buff);
-
-		// col.insertMany(mediaBuffer, (err, res) => {
-		// 	if (err) {
-		// 		console.error(err);
-		// 	}
-		// 	console.log(`chunk: ${chunkNo} / ${chunksTotal}`);
-
-		// 	//end server connection
-		// 	conn.close();
-
-			
-		// });
+    console.log('echo');
+		insertChunk(col, mediaBuffer);
 	} else {
 		//end server connection
-		conn.close();
-		
-		showEndOfProcStats();
+    conn.close();
+
+    showEndOfProcStats();
 	}
 }
 
@@ -92,37 +77,27 @@ function showEndOfProcStats() {
 }
 
 // col = targetCollection, buff = targetBuffer
-function insertChunk(col, buff) {
-	col.insertMany(buff, (err, res) => {
-		if (err) {
-			console.error(err);
-		}
-		console.log(`chunk:${chunkNo}/${chunksTotal}`);
-		chunkNo++;
-	});
-}
+function insertChunk(col) {
 
-function addChunk(collection) {
-	// insert datapoints in collection
-	// collection.insertMany(mediaBuffer, (err, res) => {
-	// 	if (err) {
-	// 		console.error(err);
-	// 	}
-	// 	console.log(`chunk: ${chunkNo} / ${chunksTotal}`);
-	// 	chunkNo++;
-	// });
+  if (mediaBuffer.length > 0) {
+    col.insertMany(mediaBuffer, (err, res) => {
+      if (err) {
+        console.error(err);
+      }
+      chunkNo++;
+      console.log(`chunk:${chunkNo}; size:${mediaBuffer.length}`);
 
-	insertChunk(collection, mediaBuffer);
-	
-	// reset buffer
-	mediaBuffer = [];
-
-	// resume reading from file
-	rl.resume();
+      // resume reading from file
+      rl.resume();
+  
+      // reset buffer
+	    mediaBuffer = [];
+    });
+  }
 }
 
 // connect to the server
-mdbclient.connect(url, (err, conn) => {
+mdbclient.connect(url, { useNewUrlParser: true }, (err, conn) => {
 
 	//create databse
 	var db = conn.db(config.db);
@@ -149,14 +124,14 @@ mdbclient.connect(url, (err, conn) => {
 				//input: fs.createReadStream('test.csv')
 			});
 
-			// Event: is emitted after each line
+      //-------Event listeners--------------------------
+			// event: is emitted after each line
 			rl.on('line', parseLine);
-
-			// Event: read stream closed
+			// event: read stream closed
 			rl.on('close', () => endActions(col, conn));
-
-			// Event: read stream paused
-			rl.on('pause', () => addChunk(col));
+			// event: read stream paused
+      rl.on('pause', () => insertChunk(col));
+      //------------------------------------------------
 		}
 	);
 });

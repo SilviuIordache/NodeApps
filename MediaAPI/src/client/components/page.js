10988ef72eb9d9051d9ed3be0ea574dad3046bce
Petre Leonard Macamete <petreleonartdos@gmail.com>
2019-07-17 16:54:53 +0300

    commit comment

--- MediaAPI/src/client/components/page.js
+++ MediaAPI/src/client/components/page.js
@@ -20,5 +20,5 @@ Vue.component('navigation', {
 
 // Define a new component called footer
 Vue.component('copyright', {
-  template: '<footer class="text-center m-5"> Caphyon 2019 </footer>'
+  template: '<footer class="text-center m-5"> //Caphyon 2019 </footer>'
 });

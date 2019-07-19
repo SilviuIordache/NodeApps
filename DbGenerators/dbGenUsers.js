df9faefda9c8871171d4088364412450eacfb4f3
Silviu <silviu.iordache92@gmail.com>
2019-07-19 11:24:21 +0300

    created a program that populates a MongoDB col with user data from a .json file

---
+++ DbGenerators/dbGenUsers.js
@@ -0,0 +1,42 @@
+const mdbclient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/';
+const fs = require('fs');
+const config = require('./config.js');
+const USERS_FILE = 'users.json';
+let users = [];
+
+
+// connect to the server
+mdbclient.connect(url, {
+    useNewUrlParser: true
+  }, (err, conn) => {
+    if (err) throw err;
+
+    //create databse
+    var db = conn.db('testDb');
+
+    // create collection
+    db.createCollection(
+      'userCollection',
+      (err, col) => {
+        if (err) {
+          conn.close();
+          return console.error(err);
+        }
+        // read from file
+        fs.readFile(USERS_FILE, 'utf8', (err, data) => {
+          if (err) throw err;
+
+          // store data in a local var
+          users = JSON.parse(data);
+          console.log(users);
+
+          // after reading from file, inser the data in the specified collection
+          db.collection('userCollection').insertMany( users, (err, res) => {
+            if (err) throw err;
+            conn.close();
+          })
+
+        })
+      });
+  });

999405091e780f838f254c51c98586678b8be793
Silviu <silviu.iordache92@gmail.com>
2019-07-22 11:52:48 +0300

    fixed asc/desc buttons to stay to not reset the current query name

--- MediaStore/src/client/components/media/filter.js
+++ MediaStore/src/client/components/media/filter.js
@@ -15,8 +15,10 @@ const filterBar = Vue.component('filter-bar', {
               v-bind:disabled='ord === "asc" ? true : false' 
               v-on:click="ord ='asc'">
               <router-link  :to="{  
-                path: '/media', 
-                query: {page: 0, ord: 'asc'}}">
+                path: '/media',
+                query: { page: 0, 
+                         ord: 'asc',
+                         name: $route.query.name}}">
                 ASC
               </router-link>
       </button>
@@ -27,7 +29,9 @@ const filterBar = Vue.component('filter-bar', {
               v-on:click="ord ='desc'">
             <router-link  :to="{  
               path: '/media', 
-              query: {page: 0, ord: 'desc'}}">
+              query: { page: 0, 
+                       ord: 'desc',
+                       name: $route.query.name}}">
               DESC
             </router-link>
       </button>
